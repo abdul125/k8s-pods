03445e9ad6f76aff55c07ee20336e00831608f55
Abdulrahman <17806978+abdul125@users.noreply.github.com>
2020-08-26 15:28:28 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-time k3d cluster create abdul -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0] -p 80:80@loadbalancer
+time k3d cluster create abdul -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0]
 kubectl create namespace test
 #git clone https://github.com/abdul125/k8s-pods
 
